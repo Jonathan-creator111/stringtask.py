ddedad66bf2ae922e30636f55e6c87086b7be161
Jonathan-creator111 <hapsjune@gmail.com>
2025-05-19 12:00:44 +0300

    first commit

---
+++ stringtask.py
@@ -0,0 +1,51 @@
+my_var = " JOHn "
+print(my_var)
+my_var=my_var.casefold()
+print(my_var)
+
+txt = " JOHn "
+print(txt)
+txt = txt.lower()
+print(txt)
+
+
+
+sentence_one = "The Dog Breed is German Shepherd"
+print(sentence_one)
+print(sentence_one[8:23])
+print(sentence_one)
+print(sentence_one[-24:-9])
+
+sentence_two = "Defeats for the Clinton forces, this was her moment of triumph"
+print(sentence_two)
+print(sentence_two[16:30])
+print(sentence_two)
+print(sentence_two[-46:-32])
+
+
+
+txt_sentence = "The lazy dog; ran so fast; it hit the wall."
+a = "The lazy dog"
+print(len(a))
+b = " ran so fast"
+print(len(b))
+c = " it hit the wall."
+print(len(c))
+
+
+
+first_name = "Joh.n"
+last_name = "Do,e"
+myVar = "Joh.n"
+myvar2 = "Do,e"
+print(myVar.replace(".","n"))
+print(myVar[0:3])
+print(myvar2.replace(",","e"))
+print(myvar2[0:2])
+full_name =myVar+" "+myvar2
+print(full_name)
+
+
+
+r = ["E","W","C"]
+print(r)
